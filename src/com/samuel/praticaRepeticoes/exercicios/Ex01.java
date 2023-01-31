0f366d3ecd94b64283dae6005f36905a0aaaa3a0
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-01-31 20:24:35 -0300

    Questao 1
    1. Faça um programa que peça uma nota, entre zero e dez. Mostre uma
    mensagem caso o valor seja inválido e continue pedindo até que o
    usuário informe um valor válido.

--- src/com/samuel/praticaRepeticoes/exercicios/Ex01.java
+++ src/com/samuel/praticaRepeticoes/exercicios/Ex01.java
@@ -1,10 +1,25 @@
 package com.samuel.praticaRepeticoes.exercicios;
 
+import java.util.Scanner;
+
 public class Ex01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner scan = new Scanner(System.in);
+		int valor = 0;
+		
+		
+		while(valor !=5 ){
+			System.out.println("Digite um numero de 0 a 10: ");
+			valor = scan.nextInt();
+			if(valor != 5) {
+				System.out.println("Errou!");
+			}else {
+				System.out.println("Acertou!!");
+			}
+			
+		}
 	}
 
 }
