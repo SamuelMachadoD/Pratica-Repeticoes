2d4e13db4ed0db516c16a6413c740b14c311d5e0
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-02-01 14:34:49 -0300

    Questao 6 - lista de numero
    6. Faça um programa que imprima na tela os números de 1 a 20, um
    abaixo do outro. Depois modifique o programa para que ele mostre os
    números um ao lado do outro.

---
+++ src/com/samuel/praticaRepeticoes/exercicios/Ex06.java
@@ -0,0 +1,21 @@
+package com.samuel.praticaRepeticoes.exercicios;
+
+import java.util.Scanner;
+
+public class Ex06 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		//Um numero acima do outro
+		for(int i = 1; i <= 20; i++) {
+			System.out.println(i);
+		}
+		
+		//um numero do lado do outro
+		for(int i = 1; i <= 20; i++) {
+			System.out.print(" " + i);
+		}
+	}
+
+}
