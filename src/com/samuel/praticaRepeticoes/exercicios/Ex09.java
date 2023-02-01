d8c4a50f91f13e7c359f6a6578c2cc53f8405126
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-02-01 14:54:34 -0300

    questao 9 - impares de 1 a 50
    9. Faça um programa que imprima na tela apenas os números ímpares
    entre 1 e 50.

---
+++ src/com/samuel/praticaRepeticoes/exercicios/Ex09.java
@@ -0,0 +1,18 @@
+package com.samuel.praticaRepeticoes.exercicios;
+
+import java.util.Scanner;
+
+public class Ex09 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		for(int i = 1; i <= 50; i++) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
