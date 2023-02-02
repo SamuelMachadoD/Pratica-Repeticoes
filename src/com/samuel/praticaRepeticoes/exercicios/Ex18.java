ea460840c8bac19f2b1ff7feeb29e18d22cf7b3d
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-02-02 14:00:03 -0300

    Questao 18
    18.Faça um programa que, dado um conjunto de N números, determine o
    menor valor, o maior valor e a soma dos valores.

--- src/com/samuel/praticaRepeticoes/exercicios/Ex18.java
+++ src/com/samuel/praticaRepeticoes/exercicios/Ex18.java
@@ -1,9 +1,34 @@
 package com.samuel.praticaRepeticoes.exercicios;
 
+import java.util.Scanner;
+
 public class Ex18 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		double maior = 0;
+		double menor = 0;
+		double soma = 0;
+		
+		for(boolean flag = true;flag;) {
+			System.out.println("Digite um numero: ( Digite 0 para encerrar )");
+			double valor = scan.nextDouble();
+			
+			if(valor != 0) {
+				if(valor > maior) {
+					maior = valor;
+				}else if(valor < menor) {
+					menor = valor;
+				}
+				soma += valor;
+			}else {
+				flag = false;
+			}
+		}
+		
+		System.out.println("O maior valor digitado foi: " + maior);
+		System.out.println("O menor valor digitado foi: " + menor);
+		System.out.println("A soma dos valores digitado é: " + soma);
 
 	}
 
