2712fde6e81f5441e2c34d64577cdce3e24b1878
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-02-02 14:46:38 -0300

    Questao 22
    22.Altere o programa de cálculo dos números primos, informando, caso o
    número não seja primo, por quais número ele é divisível.

---
+++ src/com/samuel/praticaRepeticoes/exercicios/Ex22.java
@@ -0,0 +1,26 @@
+package com.samuel.praticaRepeticoes.exercicios;
+
+import java.util.Scanner;
+
+public class Ex22 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		boolean primo = true;
+		
+		System.out.println("Digite um numero: ");
+		int valor = scan.nextInt();
+		System.out.println("Divisores:");
+		for(int i = 2; i < valor; i++) {
+			if(valor % i == 0) {
+				primo = false;
+				System.out.println(i);
+			}
+		}
+		if(primo) {
+			System.out.println("Não possui divisores, é um numero primo");
+		}else {
+			System.out.println("Não é primo");
+		}
+	}
+}
