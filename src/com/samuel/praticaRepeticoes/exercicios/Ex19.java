5db30cbc31779d87bb32860fb89b42803aa07f11
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-02-02 14:08:59 -0300

    Questao 19 e Correções
    19.Altere o programa anterior para que ele aceite apenas números entre 0
    e 1000.

--- src/com/samuel/praticaRepeticoes/exercicios/Ex19.java
+++ src/com/samuel/praticaRepeticoes/exercicios/Ex19.java
@@ -2,19 +2,19 @@ package com.samuel.praticaRepeticoes.exercicios;
 
 import java.util.Scanner;
 
-public class Ex18 {
+public class Ex19 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		double maior = 0;
-		double menor = 99999;
+		double menor = 999999;
 		double soma = 0;
 		
 		for(boolean flag = true;flag;) {
 			System.out.println("Digite um numero: ( Digite 0 para encerrar )");
 			double valor = scan.nextDouble();
 			
-			if(valor != 0) {
+			if(valor > 0 && valor < 1000) {
 				if(valor > maior) {
 					maior = valor;
 				}
@@ -22,8 +22,11 @@ public class Ex18 {
 					menor = valor;
 				}
 				soma += valor;
-			}else {
+			}else if (valor == 0){
+				System.out.println("Encerrando!");
 				flag = false;
+			}else {
+				System.out.println("Numero invalido - Digite apenas validos ( 1 - 999 )");
 			}
 		}
 		
