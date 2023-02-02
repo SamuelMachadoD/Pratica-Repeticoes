db0dc65e6c7197b96c2dfc43261024b0062ec06f
Samuel Machado Duarte <samuelmachado41@gmail.com>
2023-02-02 13:38:06 -0300

    Questao 16
    16.A série de Fibonacci é formada pela seqüência
    0,1,1,2,3,5,8,13,21,34,55,... Faça um programa que gere a série até
    que o valor seja maior que 500.

--- src/com/samuel/praticaRepeticoes/exercicios/Ex16.java
+++ src/com/samuel/praticaRepeticoes/exercicios/Ex16.java
@@ -6,6 +6,20 @@ public class Ex16 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		
+		int pre = 0, pos = 1, res = 0;
+		boolean flag = true;
+		for(;flag;) {
+			if(res <= 500) {
+				res = pre + pos;
+				pre = pos;
+				pos = res;
+				System.out.println(res);
+			}else {
+				flag = false;
+			}
+			
+		}
 
 	}
 
